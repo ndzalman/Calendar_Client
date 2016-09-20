891841d0dfda5055dce31818f5a97d3a51af06ab
Nadav Zalman <nd.zalman@gmail.com>
2016-09-20 23:09:50 +0300

    First Commit

---
+++ app/src/main/java/com/calendar_client/SignUpActivity.java
@@ -0,0 +1,59 @@
+package com.calendar_client;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private TextView tvDateOfBirth;
+    private DatePicker datePicker;
+    private int year,month,day;
+    private final static int DATE_DIALOG_ID = 999;
+    DatePickerDialog datePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        tvDateOfBirth = (TextView) findViewById(R.id.tvDateOfBirth);
+        final Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        datePicker.init(year, month, day, null);
+
+        tvDateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+
+
+         datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                showDate(year, monthOfYear + 1, dayOfMonth);
+
+                datePicker.init(year,month,day,null);
+            }},year,month,day);
+
+
+    }
+
+
+
+    private void showDate(int year,int month, int day){
+        tvDateOfBirth.setText
+                (new StringBuilder().append(day).append("/").append(month).append("/").append(year));
+    }
+}
---
+++ app/src/main/java/com/calendar_client/SplashScreenActivity.java
@@ -0,0 +1,110 @@
+package com.calendar_client;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private TextView tvAppName;
+    private ProgressBar progressBar;
+    private TextView tvStatus;
+    private AlertDialog alertDialog;
+    private final static int SPLASH_TIME=1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        tvAppName = (TextView) findViewById(R.id.tvAppName);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        tvStatus = (TextView) findViewById(R.id.tvStatus);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(R.string.alert_dialog_title)
+                .setCancelable(false)
+                .setPositiveButton(R.string.alert_dialog_positive, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new CheckConnectivityTask().execute();
+                    }
+                })
+                .setNegativeButton(R.string.alert_dialog_negative, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        finish();
+                    }
+                });
+        alertDialog = alertDialogBuilder.create();
+
+        getSupportActionBar().hide();
+
+        new CheckConnectivityTask().execute();
+
+    }
+
+    private class CheckConnectivityTask extends AsyncTask<Void,Integer,Boolean>{
+        Intent intent;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            int i = 0;
+            while (i < 6) {
+                try {
+                    Thread.sleep(SPLASH_TIME);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                i+=2;
+                publishProgress(i);
+            }
+            Boolean isConnected = isOnline();
+            return isConnected;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean isConnected) {
+            if (isConnected == false){
+                alertDialog.show();
+            }else{
+                intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            if (values[0] <= 2 ){
+                tvStatus.setText(R.string.splash_screen_status_start);
+            } else if (values[0] == 4){
+                tvStatus.setText(R.string.splash_screen_status_in_progress);
+            } else if (values[0] > 4){
+                tvStatus.setText(R.string.splash_screen_status_end);
+            }
+        }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnectedOrConnecting();
+    }
+}

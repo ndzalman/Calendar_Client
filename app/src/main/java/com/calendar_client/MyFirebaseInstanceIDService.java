0072eeaa511e435d2d55c5f3066ace8a4cfa4a5e
Nadav Zalman <nd.zalman@gmail.com>
2016-11-21 16:31:57 +0200

    Firebase Service

---
+++ app/src/main/java/com/calendar_client/MyFirebaseInstanceIDService.java
@@ -0,0 +1,27 @@
+package com.calendar_client;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Nadav on 20/11/2016.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("TEST", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+       // sendRegistrationToServer(refreshedToken);
+    }
+
+}
+
